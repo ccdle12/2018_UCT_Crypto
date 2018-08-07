04cf0819bd6fbd7cb35cde7aa263a865433bbf4f
Heewon Chung <runghyun@snu.ac.kr>
2018-08-07 17:04:08 +0900

    heewon: modified the name of variables

---
+++ ElGamal/elgamal.py
@@ -0,0 +1,23 @@
+from Crypto import Random
+from Crypto.Random import random
+from Crypto.PublicKey import ElGamal
+from Crypto.Util import number
+
+def genKey():
+    key = ElGamal.generate(160, Random.new().read)
+    return key
+
+def enc(key, msg):
+    p = key.p
+    r = number.getRandomRange(1, p-1)
+    ctxt = key.encrypt(msg, r)
+    return ctxt
+
+def mult(p, *ctxt):
+    c1 = 1
+    c2 = 1
+    for x in ctxt:
+        c1 = c1 * x[0]
+        c2 = c2 * x[1]
+    multCtxt = (c1 % p, c2 % p)
+    return multCtxt
