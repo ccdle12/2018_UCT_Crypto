9c737d8f76c3d637c7ddf0a1e91b89a74c016aa5
sumin <ssuminpark.1119@gmail.com>
2018-09-19 11:26:42 +0900

    sumin : add mult

--- Commitment/pedersen.py
+++ Commitment/pedersen.py
@@ -2,6 +2,14 @@ from Crypto import Random
 from Crypto.Random import random
 from Crypto.Util import number
 
+def generate(output):
+        q = output[1]
+        g = output[2]
+        h = output[3]
+        return q,g,h
+
+
+
 class verifier:
     def setup(self):
         p = number.getPrime(160, Random.new().read)
@@ -13,28 +21,57 @@ class verifier:
         output = (p,q,g,h)
         return output
 
-    def open(self, output, c, x, r):
+    def open(self, output, c, x, *r):
         result = "False"
-        q = output[1]
-        g = output[2]
-        h = output[3]
-        
-        r = pow(g,x,q) * pow(h,r,q)
-        if(c == r):
+        q,g,h = generate(output)
+
+        sum = 0
+        for i in r:
+            sum += i
+
+        print sum
+
+        res = (pow(g,x,q) * pow(h,sum,q)) % q
+
+        if(c == res):
             result = "True"
         return result  
 
+    def mult(self, output, *com):
+        mult = 1
+        for x in com:
+            mult *= x
+        mult = mult % output[1]
+        return mult
+        
 class prover: 
     def commit(self, output, x):
-        q = output[1]
-        g = output[2]
-        h = output[3]
+        q,g,h = generate(output)
         
         r = number.getRandomRange(1, q-1)
-        c = pow(g,x,q) * pow(h,r,q)
-        return c
+        c = (pow(g,x,q) * pow(h,r,q)) % q
+        return c, r
+    
 
+v = verifier()
+p = prover()
 
+x = 500
 
-# output = setup()
-# print output[0]
+output = v.setup()
+
+c1, r1 = p.commit(output, x)
+c2, r2 = p.commit(output, 100)
+
+mul = v.mult(output, c1, c2)
+
+# print v.open()
+
+# result1 = v.open(output, c1, x, r1)
+# result2 = v.open(output, c2, 100 , r2)
+
+# print mult 
+result = v.open(output, mul, x+100 , r1, r2)
+
+# # print c
+print result
