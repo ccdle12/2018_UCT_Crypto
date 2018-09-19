7a1bcb4b708446328793fad91f97210a15cd04d6
Heewon Chung <runghyun@snu.ac.kr>
2018-09-19 13:23:04 +0900

    heewon: changed the name of variable(s) and method(s)

--- Commitment/pedersen.py
+++ Commitment/pedersen.py
@@ -2,34 +2,30 @@ from Crypto import Random
 from Crypto.Random import random
 from Crypto.Util import number
 
-def generate(output):
-        q = output[1]
-        g = output[2]
-        h = output[3]
+def generate(param):
+        q = param[1]
+        g = param[2]
+        h = param[3]
         return q,g,h
 
-
-
 class verifier:
-    def setup(self):
-        p = number.getPrime(160, Random.new().read)
+    def setup(self, security):
+        p = number.getPrime(2 * security, Random.new().read)
         q = 2*p + 1
         g = number.getRandomRange(1, q-1)
         s = number.getRandomRange(1, q-1)
         h = pow(g,s,q)
         
-        output = (p,q,g,h)
-        return output
+        param = (p,q,g,h)
+        return param
 
-    def open(self, output, c, x, *r):
+    def open(self, param, c, x, *r):
         result = "False"
-        q,g,h = generate(output)
+        q,g,h = generate(param)
 
         sum = 0
         for i in r:
             sum += i
-
-        print sum
 
         res = (pow(g,x,q) * pow(h,sum,q)) % q
 
@@ -37,41 +33,43 @@ class verifier:
             result = "True"
         return result  
 
-    def mult(self, output, *com):
-        mult = 1
-        for x in com:
-            mult *= x
-        mult = mult % output[1]
-        return mult
+    def add(self, param, *cm):
+        addCM = 1
+        for x in cm:
+            addCM *= x
+        addCM = addCM % param[1]
+        return addCM
         
 class prover: 
-    def commit(self, output, x):
-        q,g,h = generate(output)
+    def commit(self, param, x):
+        q,g,h = generate(param)
         
         r = number.getRandomRange(1, q-1)
         c = (pow(g,x,q) * pow(h,r,q)) % q
         return c, r
     
 
+security = 80
+msg1 = 500
+msg2 = 100
+
 v = verifier()
 p = prover()
 
-x = 500
+param = v.setup(security)
 
-output = v.setup()
+c1, r1 = p.commit(param, msg1)
+c2, r2 = p.commit(param, msg2)
 
-c1, r1 = p.commit(output, x)
-c2, r2 = p.commit(output, 100)
-
-mul = v.mult(output, c1, c2)
+addCM = v.add(param, c1, c2)
 
 # print v.open()
 
-# result1 = v.open(output, c1, x, r1)
-# result2 = v.open(output, c2, 100 , r2)
+# result1 = v.open(param, c1, x, r1)
+# result2 = v.open(param, c2, 100 , r2)
 
-# print mult 
-result = v.open(output, mul, x+100 , r1, r2)
+# print add 
+result = v.open(param, addCM, msg1 + msg2 , r1, r2)
 
 # # print c
 print result
